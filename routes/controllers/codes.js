63f6dd88b3d691c09a0473e0c1bfd028af5e1219
karolsofiia <alvarezkarolsofia61@gmail.com>
2024-11-04 14:18:55 -0500

    Update codes.js

--- routes/controllers/codes.js
+++ routes/controllers/codes.js
@@ -7,7 +7,7 @@ const validateCredentials = async (req, res) => {
     const { code, userId } = req.body; // Se recibe el código y el ID del usuario desde el frontend sisi
     try {
         // Buscar si el código existe en la DB
-        const existingCode = await pool.db('sorteo').collection('codigos').findOne({ code });
+        const existingCode = await gana_como_loco('sorteo').collection('codigos').findOne({ code });
 
         if (existingCode) {
             // Verificar si el código ya ha sido usado por otro usuario
@@ -20,7 +20,7 @@ const validateCredentials = async (req, res) => {
             const userObjectId = new ObjectId(userId);
 
             // Actualizar el código en la base de datos: cambiar el estado a ocupado y asignar el userId y la fecha
-            await pool.db('sorteo').collection('codigos').updateOne(
+            await gana_como_loco('sorteo').collection('codigos').updateOne(
                 { code }, // Filtro para encontrar el código
                 { 
                     $set: { 
@@ -57,7 +57,7 @@ const getUserCodes = async (req, res) => {
     
     try {
         const userObjectId = new ObjectId(userId);  // Convertir el userId a ObjectId
-        const codes = await pool.db('sorteo').collection('codigos').find({ status: userObjectId }).toArray();
+        const codes = await gana_como_loco('sorteo').collection('codigos').find({ status: userObjectId }).toArray();
         
         res.status(200).json({ status: "Success", codes });
     } catch (error) {
@@ -69,7 +69,7 @@ const getUserCodes = async (req, res) => {
 const getWinners = async (req, res) => {
     try {
         // Busca todos los códigos con status que no sea "libre" (o sea, los códigos que han sido reclamados)
-        const winners = await pool.db('sorteo').collection('codigos').aggregate([
+        const winners = await gana_como_loco('sorteo').collection('codigos').aggregate([
             {
                 $match: { status: { $ne: 'libre' } }  // Buscar códigos ocupados
             },
